cb9350f22b7d2dd1ff52280de11e09b9966e2d8d
aernegy <jayden.chanzz06@gmail.com>
2025-03-10 09:57:10 +0800

    Session 1

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
+from os import system
 
+def main():
+    open_main = True
+
+    clear()
+
+    with open("main_menu.txt", "r") as main_menu:
+        while open_main:
+            print(main_menu)
+            user_input = input("Choose an option: ").upper()
+
+            match user_input:
+                case "1"|"2"|"3"|"Q":
+                    open_main = False
+
+                case _:
+                    clear()
+        
+    match user_input:
+        case "1":
+            clear()
+            print("Viewing")
+
+        case "2":
+            clear()
+            print("Searching")
+
+        case "3":
+            clear()
+            print("Adding")
+
+        case "Q":
+            return
+
+
+def clear():
+    system("clear")
+
+
+main()
